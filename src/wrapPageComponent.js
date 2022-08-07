8679a9e5448648f2bbc761804faa96e2c9e3914b
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-07 18:47:11 +0530

    Install SW

--- src/wrapPageComponent.js
+++ src/wrapPageComponent.js
@@ -1,12 +1,10 @@
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { install } from '@layer0/prefetch/window'
-import React, { useEffect, useState } from 'react'
 import installDevtools from '@layer0/devtools/install'
 
 export const PageComponent = ({ children }) => {
-  const [mounted, setMounted] = useState('print')
   useEffect(() => {
-    setMounted('all')
     install()
     installDevtools()
   }, [])
@@ -14,10 +12,7 @@ export const PageComponent = ({ children }) => {
     <>
       <Navbar />
       {children}
-      <link media={mounted} rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" />
-      <noscript>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" />
-      </noscript>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" />
     </>
   )
 }
