d935b542a7c7afbdadd190ac6ba09f18255d04ec
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-20 15:24:26 +0530

    Design /commerce page

--- src/templates/commerce.js
+++ src/templates/commerce.js
@@ -1,32 +1,26 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 import { filterProducts } from '../lib/helper'
-import LeftSidebar from '../components/LeftSidebar'
-import RightSidebar from '../components/RightSidebar'
 import ProductPreview from '../components/ProductPreview'
 
 const Commerce = ({ pageContext, location }) => {
   const data = Object.keys(pageContext).map((i) => pageContext[i])
   return (
-    <>
-      <div className="flex-col items-center justify-start">
-        <div className="mb-5 flex w-full flex-row items-start px-5">
-          <div className="hidden w-[15%] pt-5 md:block">
-            <LeftSidebar {...location} />
-          </div>
-          <div className="flex w-full flex-col items-start pt-5 md:w-[70%]">
-            <h2 className="text-[#FFFFFF75]">Showing {data.length} Results</h2>
-            <div className="mt-5 flex flex-row flex-wrap items-start">
-              {filterProducts(data, new URLSearchParams(location.search.substring(1)).get('filter')).map((i) => (
-                <ProductPreview key={i.path} {...i} />
-              ))}
-            </div>
-          </div>
-          <div className="hidden w-[15%] pt-5 md:block">
-            <RightSidebar {...location} />
+    <div className="flex-col items-center justify-start">
+      <div className="flex w-full flex-row items-start px-5">
+        <div className="flex min-w-[200px] flex-col pt-5">
+          <Sidebar {...location} />
+        </div>
+        <div className="flex flex-col items-start pt-5">
+          <h2 className="text-[#FFFFFF75]">Showing {data.length} Results</h2>
+          <div className="sm:grd-cols-2 mt-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+            {filterProducts(data, new URLSearchParams(location.search.substring(1)).get('filter')).map((i) => (
+              <ProductPreview key={i.path} {...i} />
+            ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
