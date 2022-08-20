b17135a71aa5eefdfbd412b33c2b78ed1459eefb
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-20 15:22:14 +0530

    Add ONE_DAY_CACHE

--- layer0/cache.js
+++ layer0/cache.js
@@ -36,47 +36,12 @@ export const IMAGE_CACHE_HANDLER = ({ cache, proxy }) => {
   proxy('image', { path: '/' })
 }
 
-export const ASSET_CACHE_HANDLER = ({ removeUpstreamResponseHeader, cache }) => {
-  // Remove the cache-control header coming in from the Next.js app,
-  // and remove the set-cookie header coming in from the Next.js app,
-  // this is to ensure that the response is cacheable
-  removeUpstreamResponseHeader('set-cookie')
-  removeUpstreamResponseHeader('cache-control')
+export const ONE_DAY_CACHE_HANDLER = ({ cache }) => {
   // Set the caching values
   cache({
     edge: {
       // Save the response(s) [whether stale or updated] in the edge POP for a year
-      maxAgeSeconds: 60 * 60 * 24 * 365,
-    },
-    browser: {
-      // Don't save the response in the browser
-      maxAgeSeconds: 0,
-      // Save the response in the browser via Layer0 service worker
-      serviceWorkerSeconds: 60 * 60 * 24,
-    },
-  })
-}
-
-export const NEXT_CACHE_HANDLER = ({ removeUpstreamResponseHeader, cache }) => {
-  // Remove the cache-control header coming in from the Next.js app,
-  // this is to ensure that the response is cacheable
-  removeUpstreamResponseHeader('cache-control')
-  // Set the caching values
-  cache({
-    browser: {
-      // Don't save the response in the browser
-      maxAgeSeconds: 0,
-      // Save the response in the browser via Layer0 service worker
-      serviceWorkerSeconds: 60 * 60 * 24,
-    },
-    edge: {
-      // Save the response(s) [whether stale or updated] in the edge POP for a year
-      maxAgeSeconds: 60 * 60 * 24 * 365,
-      // Keep revalidating data per day, i.e. looking for content changes from the Next.js app
-      // and update the response in edge
-      // More on: https://web.dev/stale-while-revalidate
-      // and https://docs.layer0.co/guides/caching#achieving-100-cache-hit-rates
-      staleWhileRevalidateSeconds: 60 * 60 * 24,
+      maxAgeSeconds: 60 * 60 * 24,
     },
   })
 }
