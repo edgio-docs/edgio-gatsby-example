1fd0737020f7361bc2637bf0d605a9c3779f9154
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-20 15:22:22 +0530

    Easy prerender requests

--- layer0/prerenderRequests.js
+++ layer0/prerenderRequests.js
@@ -8,10 +8,9 @@ export default async function getPathsToPrerender() {
   const respCall = await fetch('https://layer0-docs-layer0-ecommmerce-api-example-default.layer0-limelight.link/products/all')
   if (respCall.ok) {
     const resp = await respCall.json()
-    // For each product, derive the non slash path and create the next data url to populate on the edge
+    // For each product, derive the non slash path and create the gatsby data url to populate on the edge
     resp.forEach((i) => {
-      const nonSlashPath = i.path.replace(/\//g, '')
-      prerenderPaths.push(`/product/${nonSlashPath}`)
+      prerenderPaths.push(`/product/${i.slug}`)
     })
   } else {
     console.log('The fetch call failed, no paths to populate cache with.')
